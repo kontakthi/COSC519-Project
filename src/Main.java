20dbed44054087c482964d3f147a16956a823041
nclark3 <sponge49@hotmail.com>
2012-04-01 15:01:07 -0700

    this is another test

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@
  
  public class Main
  {
-	// This is a test bitches
+	// This is a test
  	// Input Options
  	private static final String MAIN_MENU_QUIT   = "q";
  	private static final String MAIN_MENU_USB    = "u";
